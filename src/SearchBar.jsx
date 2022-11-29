82bdc8b42998459d01e3fca450f508876cdb57fb
Lindrit <lindritsulaj2.0@gmail.com>
2022-11-29 22:13:08 +0100

    Added Searchbar

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const SearchBar = () => {
+const SearchBar = ({ searchTerm, setSearchTerm, selectedTags, setSelectedTags }) => {
   return (
     <div className="Searchbar">
-      Search Bar goes here
+      <div className='search'>
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <input type="text" placeholder='Search for your notes' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <i className="fa-solid fa-xmark" onClick={() => setSearchTerm('')}></i>
+      </div>
+      <div className='tags'>
+        <i className="fa-solid fa-tags"></i>
+        <input type="text" value={selectedTags} onChange={(e) => setSelectedTags(e.target.value)} placeholder='Tags seperated by a space' />
+      </div>
     </div>
   )
 }
